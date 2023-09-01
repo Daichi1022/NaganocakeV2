5ea33d96d989720035aade6b6a88a8aa96896834
River Nate <rivernate@ribanoiMac.local>
2023-09-01 19:32:45 +0900

    顧客側の新規登録ページ実装新規登録ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-  
  
-
-
 # namespaceを使いURL指定のパスにしたいファイル構成指定のパスにしたいを実装
-  namespace :admin do
-    get '/' => 'homes#top'
-  end
+namespace :admin do
+  get '/' => 'homes#top'
+end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+registrations: "public/registrations",
+sessions: 'public/sessions'
 }
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
+sessions: "admin/sessions"
 }
 end
+
